9abc8035d060c7acb0f345de427dfb28606030e6
Jorge Fernando Pichardo Martinez <fernandopichh@gmail.com>
2022-10-06 17:42:20 -0500

    feat(jugueteria): add new endpoint

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.routers.v1.linea_blanca import router_linea_blanca
+from app.routers.v1.jugueteria import router_jugueteria
 
 router = APIRouter()
 
@@ -8,4 +9,9 @@ router.include_router(
     router_linea_blanca,
     tags=["LINEA BLANCA"],
     prefix="/linea-blanca"
+)
+router.include_router(
+    router_jugueteria,
+    tags=["JUGUETERIA"],
+    prefix="/jugueteria"
 )
