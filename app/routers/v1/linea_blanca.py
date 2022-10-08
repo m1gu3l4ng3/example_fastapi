390bad4cd8127a4a718e8936eb11f88590959a94
Miguel Ángel García Flores <miguelangelgarciafloresti@gmail.com>
2022-10-07 20:37:52 -0500

    feat(linea_blanca): Add skip and limit

--- app/routers/v1/linea_blanca.py
+++ app/routers/v1/linea_blanca.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 router_linea_blanca = APIRouter()
 
 @router_linea_blanca.get("/")
-async def get_all_products():
+async def get_all_products(skip: int = 0, limit: int = 10):
     """
     Get all products to register
     """
